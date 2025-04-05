fb763d11012ce38e7dd751575488e131417881ae
UnUmgViolet <jaguinpaul@gmail.com>
2025-04-05 16:58:13 +0200

    fix dbl exit

--- srcs/builtins/ft_exit.c
+++ srcs/builtins/ft_exit.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_exit.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pjaguin <pjaguin@student.42.fr>            +#+  +:+       +#+        */
+/*   By: unmugviolet <unmugviolet@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/04 11:56:06 by pjaguin           #+#    #+#             */
-/*   Updated: 2025/04/04 12:03:57 by pjaguin          ###   ########.fr       */
+/*   Updated: 2025/04/05 16:57:52 by unmugviolet      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,6 @@ void	ft_exit(t_data *data, char **cmd)
 {
 	int	exit_code;
 
-	ft_putendl_fd("exit", STDOUT_FILENO);
 	if (cmd[1] && !ft_is_numeric(cmd[1]))
 	{
 		ft_putstr_fd(ERR_EXIT_SYNTAX, ERR_OUT);
