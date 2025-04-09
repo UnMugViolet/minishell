e0af2a181ff5215d375c2a140ef095e7dfdb8bf4
UnMugViolet <jaguinpaul@gmail.com>
2025-04-09 11:50:17 +0200

    Fix exit not writing on builtin

--- srcs/builtins/ft_exit.c
+++ srcs/builtins/ft_exit.c
@@ -6,7 +6,7 @@
 /*   By: unmugviolet <unmugviolet@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/04 11:56:06 by pjaguin           #+#    #+#             */
-/*   Updated: 2025/04/09 11:31:09 by unmugviolet      ###   ########.fr       */
+/*   Updated: 2025/04/09 11:41:48 by unmugviolet      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,7 @@ void	ft_exit(t_data *data, char **cmd)
 	if (cmd[1] && !ft_is_numeric(cmd[1]))
 	{
 		ft_putstr_fd(ERR_EXIT_SYNTAX, ERR_OUT);
-		ft_exit_clean(data, 255, false);
+		ft_exit_clean(data, 255, true);
 	}
 	if (cmd[1] && cmd[2])
 	{
@@ -50,5 +50,5 @@ void	ft_exit(t_data *data, char **cmd)
 	}
 	else
 		exit_code = 0;
-	ft_exit_clean(data, exit_code, false);
+	ft_exit_clean(data, exit_code, true);
 }
